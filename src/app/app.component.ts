1aa4c5d58122dc8abaff19bec90d7c88763e6416
Luz <aracely.des@uanl.edu.mx>
2022-02-26 13:33:54 -0600

    Inicializar proyecto y agregar un nuevo componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-raiz',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'ionic-angular-1FM045';
+  total= 90;
+
+  esCierto = true;
+
+  imagenes = [
+    "assets/perrillo.jpeg",
+    "assets/perrillo.jpeg",
+    "assets/perrillo.jpeg",
+    "assets/perrillo.jpeg",
+    "assets/perrillo.jpeg"
+  ];
+
+  
 }
---
+++ src/app/feed/feed.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feed',
+  templateUrl: './feed.component.html',
+  styleUrls: ['./feed.component.css']
+})
+export class FeedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  perrillos = [
+    {
+      "usuario": "@unosperrillos",
+      "src": "assets/perrillo.jpeg",
+      "caption": "unos perrillos...."
+    },
+    {
+      "usuario": "@amsiedad",
+      "src": "assets/amsiedad.jpeg",
+      "caption": "me da amsiedad"
+    },
+  ]
+
+}
