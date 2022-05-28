03b90f63b8bc3bc7efad70db2a7f07490e4bdd6d
Your Name <johann.velazquezantn@uanl.edu.mx>
2022-05-28 13:49:55 -0500

    tomar foto

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource, Photo} from '@capacitor/camera'
+
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,22 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  img: string= '';
+
   constructor() {}
+
+  async tomarFoto(){
+    const foto = await Camera.getPhoto({
+      quality: 100,
+      resultType: CameraResultType.Base64,
+      source: CameraSource.Camera
+    })
+  
+    if(foto){
+      this.img = 'data:image/jpeg;base64,'+ foto.base64String;
+    }
+  
+  }
+  
 
 }
